2a654da8060eb45f7760b22ae3cc42cc27b42f1b
Steve Fake <steven.m.fake@gmail.com>
2016-03-10 07:31:09 -0500

    Moved some lines to comment hashes

--- blackjack_advisor.rb
+++ blackjack_advisor.rb
@@ -529,51 +529,51 @@ end
 
 #user enter "J", "Q", or "K" for Jack, Queen, or King, enter A for an Ace.
 
-
-Player	Dealer's Card
-Hard	2	3	4	5	6	7	8	9	10	A
-5	    H	H	H	H	H	H	H	H	H	H
-6	    H	H	H	H	H	H	H	H	H	H
-7	    H	H	H	H	H	H	H	H	H	H
-8	    H	H	H	Dh	Dh	H	H	H	H	H
-9	    Dh	Dh	Dh	Dh	Dh	H	H	H	H	H
-10	  Dh	Dh	Dh	Dh	Dh	Dh	Dh	Dh	H	H
-11	  Dh	Dh	Dh	Dh	Dh	Dh	Dh	Dh	Dh	Dh
-12	  H	H	S	S	S	H	H	H	H	H
-13	  S	S	S	S	S	H	H	H	H	H
-14	  S	S	S	S	S	H	H	H	H	H
-15  	S	S	S	S	S	H	H	H	H	H
-16  	S	S	S	S	S	H	H	H	H	H
-17  	S	S	S	S	S	S	S	S	S	S
-18  	S	S	S	S	S	S	S	S	S	S
-19  	S	S	S	S	S	S	S	S	S	S
-20  	S	S	S	S	S	S	S	S	S	S
-21  	S	S	S	S	S	S	S	S	S	S
-
-Soft	2	3	4	5	6	7	8	9	10	A
-13	H	H	Dh	Dh	Dh	H	H	H	H	H
-14	H	H	Dh	Dh	Dh	H	H	H	H	H
-15	H	H	Dh	Dh	Dh	H	H	H	H	H
-16	H	H	Dh	Dh	Dh	H	H	H	H	H
-17	Dh	Dh	Dh	Dh	Dh	H	H	H	H	H
-18	S	Ds	Ds	Ds	Ds	S	S	H	H	S
-19	S	S	S	S	Ds	S	S	S	S	S
-20	S	S	S	S	S	S	S	S	S	S
-21	S	S	S	S	S	S	S	S	S	S
-Pair	2	3	4	5	6	7	8	9	10	A
-2,2	P	P	P	P	P	P	H	H	H	H
-3,3	P	P	P	P	P	P	P	H	H	H
-4,4	H	H	P	P	P	H	H	H	H	H
-5,5	Dh	Dh	Dh	Dh	Dh	Dh	Dh	Dh	H	H
-6,6	P	P	P	P	P	P	H	H	H	H
-7,7	P	P	P	P	P	P	P	H	S	H
-8,8	P	P	P	P	P	P	P	P	P	P
-9,9	P	P	P	P	P	S	P	P	S	S
-10,10	S	S	S	S	S	S	S	S	S	S
-A,A	P	P	P	P	P	P	P	P	P	P
-
-H	Hit
-S	Stand
-P	Split
-Dh	Double if possible, otherwise Hit
-Ds	Double if possible, otherwise Stand
+#
+# Player	Dealer's Card
+# Hard	2	3	4	5	6	7	8	9	10	A
+# 5	    H	H	H	H	H	H	H	H	H	H
+# 6	    H	H	H	H	H	H	H	H	H	H
+# 7	    H	H	H	H	H	H	H	H	H	H
+# 8	    H	H	H	Dh	Dh	H	H	H	H	H
+# 9	    Dh	Dh	Dh	Dh	Dh	H	H	H	H	H
+# 10	  Dh	Dh	Dh	Dh	Dh	Dh	Dh	Dh	H	H
+# 11	  Dh	Dh	Dh	Dh	Dh	Dh	Dh	Dh	Dh	Dh
+# 12	  H	H	S	S	S	H	H	H	H	H
+# 13	  S	S	S	S	S	H	H	H	H	H
+# 14	  S	S	S	S	S	H	H	H	H	H
+# 15  	S	S	S	S	S	H	H	H	H	H
+# 16  	S	S	S	S	S	H	H	H	H	H
+# 17  	S	S	S	S	S	S	S	S	S	S
+# 18  	S	S	S	S	S	S	S	S	S	S
+# 19  	S	S	S	S	S	S	S	S	S	S
+# 20  	S	S	S	S	S	S	S	S	S	S
+# 21  	S	S	S	S	S	S	S	S	S	S
+#
+# Soft	2	3	4	5	6	7	8	9	10	A
+# 13	H	H	Dh	Dh	Dh	H	H	H	H	H
+# 14	H	H	Dh	Dh	Dh	H	H	H	H	H
+# 15	H	H	Dh	Dh	Dh	H	H	H	H	H
+# 16	H	H	Dh	Dh	Dh	H	H	H	H	H
+# 17	Dh	Dh	Dh	Dh	Dh	H	H	H	H	H
+# 18	S	Ds	Ds	Ds	Ds	S	S	H	H	S
+# 19	S	S	S	S	Ds	S	S	S	S	S
+# 20	S	S	S	S	S	S	S	S	S	S
+# 21	S	S	S	S	S	S	S	S	S	S
+# Pair	2	3	4	5	6	7	8	9	10	A
+# 2,2	P	P	P	P	P	P	H	H	H	H
+# 3,3	P	P	P	P	P	P	P	H	H	H
+# 4,4	H	H	P	P	P	H	H	H	H	H
+# 5,5	Dh	Dh	Dh	Dh	Dh	Dh	Dh	Dh	H	H
+# 6,6	P	P	P	P	P	P	H	H	H	H
+# 7,7	P	P	P	P	P	P	P	H	S	H
+# 8,8	P	P	P	P	P	P	P	P	P	P
+# 9,9	P	P	P	P	P	S	P	P	S	S
+# 10,10	S	S	S	S	S	S	S	S	S	S
+# A,A	P	P	P	P	P	P	P	P	P	P
+#
+# H	Hit
+# S	Stand
+# P	Split
+# Dh	Double if possible, otherwise Hit
+# Ds	Double if possible, otherwise Stand
